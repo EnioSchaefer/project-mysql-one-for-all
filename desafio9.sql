690e739bcc1f75b7799a5191fc4c657fbd1f9499
Enio Schaefer <enio100schaefer@hotmail.com>
2023-01-27 01:28:34 -0300

    [Feat] :rocket: Requisito 09 Concluido

---
+++ desafio9.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(h.history_music) AS 'quantidade_musicas_no_historico'
+FROM SpotifyClone.history AS h 
+INNER JOIN SpotifyClone.users AS u ON h.user_id = u.user_id
+WHERE u.user_name = 'Barbara Liskov';
